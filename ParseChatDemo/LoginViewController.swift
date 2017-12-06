880a4db5a986cdf36dba150368d17fafbae97532
bzsinger <benjamin.z.singer@gmail.com>
2017-12-06 15:07:55 -0600

    Add signup and login functionality

--- ParseChatDemo/LoginViewController.swift
+++ ParseChatDemo/LoginViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +25,47 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func signupButtonTapped(_ sender: Any) {
+        registerUser()
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        loginUser()
+    }
+    
+    func registerUser() {
+        // initialize a user object
+        let newUser = PFUser()
+        
+        // set user properties
+        newUser.username = usernameTextField.text
+        newUser.password = passwordTextField.text
+        
+        // call sign up function on the object
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("User Registered successfully")
+                // manually segue to logged in view
+            }
+        }   
+    }
+    
+    func loginUser() {
+        
+        let username = usernameTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user: PFUser?, error: Error?) in
+            if let error = error {
+                print("User log in failed: \(error.localizedDescription)")
+            } else {
+                print("User logged in successfully")
+                // display view controller that needs to shown after successful login
+            }
+        }   
+    }
     /*
     // MARK: - Navigation
 
